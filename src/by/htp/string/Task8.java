1b993ea724eaed32ad62b3fe3b10d20abe21e1e7
alexsolovyova <solovyovaa@yahoo.com>
2017-04-06 20:40:48 +0300

    task

--- src/by/htp/string/Task8.java
+++ src/by/htp/string/Task8.java
@@ -1,15 +1,26 @@
 package by.htp.string;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class Task8 {
 
 	public static void main(String[] args) {
-			String s = "a b c d (e)";
-char a = '*';
+			String s = "Everybody (lies)";
+
 			
 			String [] str = s.split(" ");
-			StringBuilder sb = new StringBuilder();
-			
-			
-
+			StringBuffer sb = new StringBuffer();
+	        
+	        
+	        
+	        Pattern p = Pattern.compile("^[(].*[)]$");
+	        for (String string : str) {
+	        Matcher m = p.matcher(string);
+	            if(!m.matches()){
+	                sb.append(string);
+	                sb.append(" ");
+	            }
+	            
+	        }
+	        System.out.println(sb.toString());
+	    }
 	}
-}
